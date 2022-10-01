3ad86c25a84ea8920f9bc24d314fa9cac892223c
Amir Yanny <ayanny@users.noreply.github.com>
2022-10-01 13:19:49 -0600

    Update boundary.tf

--- 01-vpc/boundary.tf
+++ 01-vpc/boundary.tf
@@ -3,10 +3,10 @@ resource "tls_private_key" "dodworkshop" {
   rsa_bits  = 4096
 }
 
-#resource "aws_key_pair" "generated_key" {
- # key_name   = "dodworkshop"
-  #public_key = tls_private_key.dodworkshop.public_key_openssh
-#}
+resource "aws_key_pair" "generated_key" {
+  key_name   = "dodworkshop"
+  public_key = tls_private_key.dodworkshop.public_key_openssh
+}
 
 resource "aws_security_group_rule" "allow_inbound_ssh" {
   type        = "ingress"
@@ -33,7 +33,7 @@ resource "aws_instance" "boundary" {
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [module.vpc.default_security_group_id]
-#  key_name                    = aws_key_pair.generated_key.key_name
+  key_name                    = aws_key_pair.generated_key.key_name
   subnet_id                   = module.vpc.public_subnets[0]
   user_data_base64            = base64encode(file("user_data_boundary.sh"))
   tags = {
